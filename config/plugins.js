b1e717bd8fba51a7968ab6ac05a9b23094b22360
Harry <harald.innetzberger@web.de>
2023-06-15 17:25:39 +0200

    inital commit

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+        enabled: false,
+        /*
+        config: {
+          deployHook:
+            "https://api.vercel.com/v1/integrations/deploy/prj_<deploy-hook>",
+          apiToken: "<vercel-api-token>",
+          appFilter: "your-app-name-on-vercel",
+          teamFilter: "your-team-id-on-vercel",
+          roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+        },
+        */
+      },
+  });
